d1fa7b2245407a55aeca6f12d8477e85e7c65eab
honshin4n1 <million_films.sh@icloud.com>
2020-12-09 20:51:36 +0900

    shop model migrate

--- db/migrate/20201209114617_create_shops.rb
+++ db/migrate/20201209114617_create_shops.rb
@@ -1,7 +1,9 @@
 class CreateShops < ActiveRecord::Migration[5.2]
   def change
     create_table :shops do |t|
-
+      t.string :address
+      t.float  :latitude
+      t.float  :longitude
       t.timestamps
     end
   end
