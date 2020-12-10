3d827b5d3f5124ee6a9d4fe905cab13f0a2ff34c
honshin4n1 <million_films.sh@icloud.com>
2020-12-10 15:19:00 +0900

    Update maps_controller.rb

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,16 +1,33 @@
 class MapsController < ApplicationController
   def index
+    @map = Map.new
   end
 
-  def map
-    # respond_to以下の記述によって、
-    # remote: trueのアクセスに対して、
-    # map.js.erbが変えるようになります。
-    results = Geocoder.search(params[:address])
-    @latlng = results.first.coordinates
-    # これでmap.js.erbで、経度緯度情報が入った@latlngを使える。
-    respond_to do |format|
-      format.js
-    end
+  def create
+    @map = Map.new(map_params)
+    @map.save
+    redirect_to root_path
+  end
+
+  def show
+    @map = Map.find_by(params[:id])
+  end
+
+  # def map
+  #   # respond_to以下の記述によって、
+  #   # remote: trueのアクセスに対して、
+  #   # map.js.erbが変えるようになります。
+  #   results = Geocoder.search(params[:address])
+  #   @latlng = results.first.coordinates
+  #   # これでmap.js.erbで、経度緯度情報が入った@latlngを使える。
+  #   respond_to do |format|
+  #     format.js
+  #   end
+    # end
+
+  private
+
+  def map_params
+    params.permit(:address)
   end
 end
